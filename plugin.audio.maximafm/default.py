f66925cbe2e3fcbed40c091129ec7172ebb9c103
Jebiveter <drago.kraner@gmail.com>
2016-03-01 09:53:21 +0100

    Error install Fix

--- plugin.audio.maximafm/default.py
+++ plugin.audio.maximafm/default.py
@@ -32,47 +32,42 @@ from xbmcplugin import addDirectoryItem, endOfDirectory
 
 MAXIMAFM_STREAM = 'http://5243.live.streamtheworld.com:80/MAXIMAFM_SC'
 
----> [playlist]
-File1=http://13413.live.streamtheworld.com:443/MAXIMAFM_SC
-File2=http://13413.live.streamtheworld.com:3690/MAXIMAFM_SC
-File3=http://13413.live.streamtheworld.com:80/MAXIMAFM_SC
-File4=http://13393.live.streamtheworld.com:443/MAXIMAFM_SC
-File5=http://13393.live.streamtheworld.com:3690/MAXIMAFM_SC
-File6=http://13393.live.streamtheworld.com:80/MAXIMAFM_SC
-File7=http://13873.live.streamtheworld.com:443/MAXIMAFM_SC
-File8=http://13873.live.streamtheworld.com:3690/MAXIMAFM_SC
-File9=http://13873.live.streamtheworld.com:80/MAXIMAFM_SC
-File10=http://10883.live.streamtheworld.com:443/MAXIMAFM_SC
-File11=http://10883.live.streamtheworld.com:3690/MAXIMAFM_SC
-File12=http://10883.live.streamtheworld.com:80/MAXIMAFM_SC
-File13=http://13303.live.streamtheworld.com:443/MAXIMAFM_SC
-File14=http://13303.live.streamtheworld.com:3690/MAXIMAFM_SC
-File15=http://13303.live.streamtheworld.com:80/MAXIMAFM_SC
-File16=http://5293.live.streamtheworld.com:443/MAXIMAFM_SC
-File17=http://5293.live.streamtheworld.com:3690/MAXIMAFM_SC
-File18=http://5293.live.streamtheworld.com:80/MAXIMAFM_SC
-File19=http://10913.live.streamtheworld.com:443/MAXIMAFM_SC
-File20=http://10913.live.streamtheworld.com:3690/MAXIMAFM_SC
-File21=http://10913.live.streamtheworld.com:80/MAXIMAFM_SC
-File22=http://13863.live.streamtheworld.com:443/MAXIMAFM_SC
-File23=http://13863.live.streamtheworld.com:3690/MAXIMAFM_SC
-File24=http://13863.live.streamtheworld.com:80/MAXIMAFM_SC
-File25=http://10923.live.streamtheworld.com:443/MAXIMAFM_SC
-File26=http://10923.live.streamtheworld.com:3690/MAXIMAFM_SC
-File27=http://10923.live.streamtheworld.com:80/MAXIMAFM_SC
-File28=http://13373.live.streamtheworld.com:443/MAXIMAFM_SC
-File29=http://13373.live.streamtheworld.com:3690/MAXIMAFM_SC
-File30=http://13373.live.streamtheworld.com:80/MAXIMAFM_SC
-File31=http://5243.live.streamtheworld.com:443/MAXIMAFM_SC
-File32=http://5243.live.streamtheworld.com:3690/MAXIMAFM_SC
-File33=http://5243.live.streamtheworld.com:80/MAXIMAFM_SC
-Title1=MAXIMAFM_SC
-Title2=MAXIMAFM_SC-Bak
-Length1=-1
-NumberOfEntries=33
-Version=2
-
-<---
+#---> [playlist]
+# File1=http://13413.live.streamtheworld.com:443/MAXIMAFM_SC
+# File2=http://13413.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File3=http://13413.live.streamtheworld.com:80/MAXIMAFM_SC
+# File4=http://13393.live.streamtheworld.com:443/MAXIMAFM_SC
+# File5=http://13393.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File6=http://13393.live.streamtheworld.com:80/MAXIMAFM_SC
+# File7=http://13873.live.streamtheworld.com:443/MAXIMAFM_SC
+# File8=http://13873.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File9=http://13873.live.streamtheworld.com:80/MAXIMAFM_SC
+# File10=http://10883.live.streamtheworld.com:443/MAXIMAFM_SC
+# File11=http://10883.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File12=http://10883.live.streamtheworld.com:80/MAXIMAFM_SC
+# File13=http://13303.live.streamtheworld.com:443/MAXIMAFM_SC
+# File14=http://13303.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File15=http://13303.live.streamtheworld.com:80/MAXIMAFM_SC
+# File16=http://5293.live.streamtheworld.com:443/MAXIMAFM_SC
+# File17=http://5293.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File18=http://5293.live.streamtheworld.com:80/MAXIMAFM_SC
+# File19=http://10913.live.streamtheworld.com:443/MAXIMAFM_SC
+# File20=http://10913.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File21=http://10913.live.streamtheworld.com:80/MAXIMAFM_SC
+# File22=http://13863.live.streamtheworld.com:443/MAXIMAFM_SC
+# File23=http://13863.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File24=http://13863.live.streamtheworld.com:80/MAXIMAFM_SC
+# File25=http://10923.live.streamtheworld.com:443/MAXIMAFM_SC
+# File26=http://10923.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File27=http://10923.live.streamtheworld.com:80/MAXIMAFM_SC
+# File28=http://13373.live.streamtheworld.com:443/MAXIMAFM_SC
+# File29=http://13373.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File30=http://13373.live.streamtheworld.com:80/MAXIMAFM_SC
+# File31=http://5243.live.streamtheworld.com:443/MAXIMAFM_SC
+# File32=http://5243.live.streamtheworld.com:3690/MAXIMAFM_SC
+# File33=http://5243.live.streamtheworld.com:80/MAXIMAFM_SC
+#Title1=MAXIMAFM_SC
+#<---
 
 class StreamPlayer:
 
